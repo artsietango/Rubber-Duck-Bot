72914a10dac0fae992a75de15a3d72877ae1570e
artsietango <rosetardis99@gmail.com>
2022-06-24 21:53:52 -0500

    prompt test

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				to: channelID,
 				message: 'pong!'
 			});
-			// Just add any case commands if you want to..
+			// /
 		}
 	}
 });
